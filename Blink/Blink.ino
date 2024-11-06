ae70738e4174096fc3037d7ec01fc8b95d736a7f
Elias Hohl <elias.hohl@ehtec.co>
2024-11-06 02:23:52 +0100

    Use external LED

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,13 +1,16 @@
+const int LED_EXTERNAL = 13;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_EXTERNAL, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+  digitalWrite(LED_EXTERNAL, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_EXTERNAL, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
