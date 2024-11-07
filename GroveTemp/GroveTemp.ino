9d709d29444f6b38208b917edebaac134a81df68
Elias Hohl <elias.hohl@ehtec.co>
2024-11-08 00:52:41 +0100

    Full Grove Temperature sensor code

---
+++ GroveTemp/GroveTemp.ino
@@ -0,0 +1,78 @@
+#include <math.h>
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+const int ROTARY_ANGLE_SENSOR = A1;
+const float ADC_REF = 5.0; //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino
+                    //board switches to 3V3, the ADC_REF should be 3.3
+const float GROVE_VCC = 5.0; //VCC of the grove interface is normally 5v
+const float FULL_ANGLE = 300.0; //full value of the rotary angle is 300 degrees
+
+const float B = 4275;               // B value of the thermistor
+const float R0 = 100000;            // R0 = 100k
+const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
+
+rgb_lcd lcd;
+
+const int colorR = 0;
+const int colorG = 255;
+const int colorB = 0;
+
+#if defined(ARDUINO_ARCH_AVR)
+#define debug  Serial
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+#define debug  SerialUSB
+#else
+#define debug  Serial
+#endif
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(ROTARY_ANGLE_SENSOR, INPUT);
+
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+
+}
+
+void loop()
+{
+    // Temperature reading
+    int a = analogRead(pinTempSensor);
+
+    float R = 1023.0 / a - 1.0;
+    R = R0 * R;
+
+    float temperature = 1.0 / (log(R / R0) / B + 1 / 298.15) - 273.15; // Convert to temperature via datasheet
+
+    // Target temperature reading
+    float voltage;
+    int sensor_value = analogRead(ROTARY_ANGLE_SENSOR);
+    voltage = (float)sensor_value * ADC_REF / 1023.0;
+    float degrees = (voltage * FULL_ANGLE) / GROVE_VCC;
+    float target;
+    target = 20.0 + (degrees * 20.0 / FULL_ANGLE);
+
+    // Send temperature and target to serial port on the same line separated by a tab
+    Serial.print(temperature);
+    Serial.print(",");
+    Serial.println(target);
+
+    // Display both values on the LCD
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temperature);
+    lcd.print(" C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Target: ");
+    lcd.print(target);
+    lcd.print(" C");
+
+    delay(100);
+}
